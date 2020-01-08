d0f3624508bc59c3eb94d3c3e05f999d2ec21da8
Sergio <Sergio@192.168.2.152>
2020-01-07 21:02:17 -0500

    update 2

--- src/human.java
+++ src/human.java
@@ -1,39 +1,16 @@
-import org.newdawn.slick.Animation;
-import org.newdawn.slick.Image;
+
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.SpriteSheet;
 
-public class human extends soldier {
-    private SpriteSheet humspr;
-    private Image walk[] = new Image[4];
-    private Animation ani;
-    public human(int x, int y) throws SlickException {
-        super(x, y);
-        humspr = new SpriteSheet("data/lof.png",192,192);
-        humspr.startUse();
-        walk[0] = humspr.getSubImage(0,0);
-        walk[1] = humspr.getSubImage(0,1);
-        walk[2] = humspr.getSubImage(0,2);
-        walk[3] = humspr.getSubImage(1,0);
-        
-        
-        ani = new Animation(walk,100);
-        
-        humspr.endUse();
-        
-    }
-    
-    
 
-    @Override
-    public void makerect() {
-        
+public class human extends soldier{
+
+    public human(int x, int y, String s) throws SlickException {
+        super(x, y, s);
     }
 
     @Override
-    public void draw() {
-        ani.start();
-        ani.draw(xloc,yloc,hitbox.getWidth(),hitbox.getHeight());
+    public void attackdmg() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
--- src/soldier.java
+++ src/soldier.java
@@ -1,16 +1,34 @@
+
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 import org.newdawn.slick.SpriteSheet;
 import org.newdawn.slick.geom.Rectangle;
 
 public abstract class soldier {
+
     protected Rectangle hitbox;
     protected int xloc, yloc;
-    
-    public soldier(int x, int y){
-    makerect();
-    xloc = x;
-    yloc = y;
-}
-abstract public void makerect();
-abstract public void draw();
-}
+    protected SpriteSheet humspr;
+    protected Image walk[] = new Image[4];
+    protected Animation ani;
 
+    public soldier(int x, int y, String s) throws SlickException {
+        humspr = new SpriteSheet(s, 192, 192);
+        humspr.startUse();
+        walk[0] = humspr.getSubImage(0, 0);
+        walk[1] = humspr.getSubImage(0, 1);
+        walk[2] = humspr.getSubImage(0, 2);
+        walk[3] = humspr.getSubImage(1, 0);
+        ani = new Animation(walk, 100);
+        humspr.endUse();
+        hitbox = new Rectangle(x, y, walk[0].getWidth(), walk[0].getHeight());
+    }
+
+    abstract public void attackdmg();
+
+    public void draw() {
+        ani.start();
+        ani.draw(hitbox.getX(),hitbox.getY());
+    }
+}
