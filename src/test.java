6d07b49e685ea9fab1606d198343d6f9331c6bdc
Sergio <Sergio@192.168.2.152>
2020-01-07 21:01:59 -0500

    update 2

--- src/test.java
+++ src/test.java
@@ -1,30 +1,33 @@
+
 import org.newdawn.slick.*;
 
 public class test extends BasicGame {
 
-public test (String title) {
-super(title);
-}
+    soldier s;
 
-public void init(GameContainer gc) throws SlickException {
+    public test(String title) {
+        super(title);
+    }
 
-}
+    public void init(GameContainer gc) throws SlickException {
+        s = new human(100,100,"data/lof.png");
+    }
 
-public void update(GameContainer gc, int i) throws SlickException {
+    public void update(GameContainer gc, int i) throws SlickException {
 
-}
+    }
 
-public void render(GameContainer gc, Graphics g) throws SlickException {
+    public void render(GameContainer gc, Graphics g) throws SlickException {
+        s.draw();
+    }
 
-}
-
-public static void main(String args[]) throws SlickException {
-  test game = new test("Testing Game");
-  AppGameContainer app = new AppGameContainer(game);
-  app.setDisplayMode(800, 600, false);
-  app.setShowFPS(false);
-  app.setTargetFrameRate(100);
-  app.start();
-}
+    public static void main(String args[]) throws SlickException {
+        test game = new test("Testing Game");
+        AppGameContainer app = new AppGameContainer(game);
+        app.setDisplayMode(800, 600, false);
+        app.setShowFPS(false);
+        app.setTargetFrameRate(100);
+        app.start();
+    }
 
 }
